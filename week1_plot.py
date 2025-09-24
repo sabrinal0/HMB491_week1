6f122bc4f1231b75831ddd79e13c521246ffb10c
Sabrina Lo <sabrinalo@MacBook-Pro-21.local>
2025-09-24 16:20:18 -0400

    Add scatterplot for mammal_teeth dataset with title including name

--- week1_plot.py
+++ week1_plot.py
@@ -2,12 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # TODO: load the dataset as pandas dataframe
+df_teeth = pd.read_csv("mammal_teeth.csv")
 
-plt.figure(figsize=(5, 10)) # set figure size
-plt.scatter(x=df_teeth['Top incisors'],
-y=df_teeth['MAMMAL']) # set figure x, y axis
+plt.figure(figsize=(5, 10))  # set figure size
+plt.scatter(
+    x=df_teeth['Top incisors'],
+    y=df_teeth['MAMMAL']
+)
 plt.gca().xaxis.set_visible(False)
 
 # TODO: change the title name to include your name
-plt.title("plot for mammal_teeth dataset")
-plt.savefig("mammal_teeth_scatterplot.png", dpi=150) # save the figure
+plt.title("plot for mammal_teeth dataset - Sabrina Lo")
+plt.savefig("mammal_teeth_scatterplot.png", dpi=150)  # save the figure
